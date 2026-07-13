3f63d0da085802b260478301a694e815ec4374b3
Mayank Jha <154996743+Mayank87426@users.noreply.github.com>
2026-07-14 03:20:03 +0530

    Time: 4 ms (62.42%), Space: 11.4 MB (51.43%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    long long power(long long a, long long b) {
-        long long result = 1;
-        while (b > 0) {
-            if (b & 1)
-                result *= a;
-            a *= a;
-            b >>= 1;
-        }
-        return result;
-    }
     int nthUglyNumber(int n) {
-        unordered_set<long long> nums;
-        for (int i = 0; i <= 30; i++) {
-            for (int j = 0; j <= 20; j++) {
-                for (int k = 0; k <= 15; k++) {
-                    long long a = power(2, i);
-                    long long b = power(3, j);
-                    long long c = power(5, k);
-                    if (a > INT_MAX / b)
-                        continue;
-                    long long ab = a * b;
-                    if (ab > INT_MAX / c)
-                        continue;
-                    long long val = ab * c;
-                    nums.insert(val);
-
-                }
-            }
+        vector<int> t(n+1);
+        t[1] = 1; 
+        
+        int i2;
+        int i3; 
+        int i5; 
+        i2 = i3 = i5 = 1;
+        
+        for(int i = 2; i<=n; i++) {
+            int i2th_ugly = t[i2] * 2;
+            
+            int i3rd_ugly = t[i3] * 3;
+            
+            int i5th_ugly = t[i5] * 5;
+            
+            t[i] = min({i2th_ugly, i3rd_ugly, i5th_ugly});
+            
+            if(t[i] == i2th_ugly)
+                i2++;
+            
+            if(t[i] == i3rd_ugly)
+                i3++;
+            
+            if(t[i] == i5th_ugly)
+                i5++;
         }
-        vector<long long> v;
-        for (auto it : nums)
-            v.push_back(it);
-        sort(begin(v), end(v));
-        return v[n - 1];
+        
+        return t[n];
     }
 };
