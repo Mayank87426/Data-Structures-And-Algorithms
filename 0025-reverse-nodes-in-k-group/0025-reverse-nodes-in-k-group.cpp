340f0971f4634eff257ebfb3890c49fe7cac5f3c
Mayank Jha <154996743+Mayank87426@users.noreply.github.com>
2026-07-23 08:34:56 +0530

    Time: 2 ms (3.43%), Space: 16.4 MB (71.32%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,40 +1,55 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(next) {}
- * };
- */
 class Solution {
 public:
-    // Reverse exactly k nodes
-    ListNode* reverseLL(ListNode* head, int k, ListNode* prev) {
-        if (k == 0)
-            return prev;
+    ListNode* getKthNode(ListNode* head, int k) {
+        while (head && k > 1) {
+            head = head->next;
+            k--;
+        }
+        return head;
+    }
 
-        ListNode* next = head->next;
-        head->next = prev;
-        return reverseLL(next, k - 1, head);
+    void reverseLL(ListNode*& head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        head = prev;
     }
 
     ListNode* reverseKGroup(ListNode* head, int k) {
-        // Check whether there are at least k nodes
         ListNode* temp = head;
-        for (int i = 0; i < k; i++) {
-            if (temp == NULL)
-                return head;
-            temp = temp->next;
+        ListNode* prevNode = nullptr;
+
+        while (temp) {
+            ListNode* kthNode = getKthNode(temp, k);
+
+            if (!kthNode) {
+                if (prevNode)
+                    prevNode->next = temp;
+                break;
+            }
+
+            ListNode* nextNode = kthNode->next;
+            kthNode->next = nullptr;
+
+            ListNode* groupStart = temp;
+            reverseLL(groupStart);
+
+            if (temp == head)
+                head = groupStart;
+            else
+                prevNode->next = groupStart;
+
+            prevNode = temp; 
+            temp = nextNode;
         }
 
-        // temp now points to the node after the k-group
-        ListNode* newHead = reverseLL(head, k, temp);
-
-        // head becomes the tail after reversal
-        head->next = reverseKGroup(temp, k);
-
-        return newHead;
+        return head;
     }
 };
