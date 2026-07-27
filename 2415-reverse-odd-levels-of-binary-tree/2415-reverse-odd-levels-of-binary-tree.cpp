3831232a77cc84e139cb16fd409b9a6e3c40bd14
Mayank Jha <154996743+Mayank87426@users.noreply.github.com>
2026-07-27 09:22:51 +0530

    Time: 0 ms (100%), Space: 80.4 MB (63.21%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
@@ -1,66 +1,30 @@
 class Solution {
 public:
-    vector<vector<TreeNode*>> levelOrder(TreeNode* root) {
-        vector<vector<TreeNode*>> traversal;
-        if (!root)
-            return traversal;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int n = q.size();
-            vector<TreeNode*> currLevel;
-
-            while (n--) {
-                TreeNode* node = q.front();
-                q.pop();
-
-                currLevel.push_back(node);
-
-                if (node->left)
-                    q.push(node->left);
-                if (node->right)
-                    q.push(node->right);
-            }
-
-            traversal.push_back(currLevel);
-        }
-
-        return traversal;
-    }
-
     void oddLevelReverse(TreeNode* root) {
-        auto levelOrderTraversal = levelOrder(root);
         queue<TreeNode*> q;
         q.push(root);
         int level = 0;
-
+        vector<int> prev;
         while (!q.empty()) {
             int n = q.size();
-
-            vector<int> vals;
-            if (level % 2) {
-                for (auto node : levelOrderTraversal[level])
-                    vals.push_back(node->val);
-            }
-
-            int i = vals.size() - 1;
-
             while (n--) {
-                TreeNode* node = q.front();
+                auto node = q.front();
                 q.pop();
-
-                if (level % 2) {
-                    node->val = vals[i--];
-                }
-
-                if (node->left)
+                if (node->left) {
                     q.push(node->left);
-                if (node->right)
+                    if (level % 2 == 0)
+                        prev.push_back(node->left->val);
+                }
+                if (node->right) {
                     q.push(node->right);
+                    if (level % 2 == 0)
+                        prev.push_back(node->right->val);
+                }
+                if (level % 2 == 1) {
+                    node->val = prev.back();
+                    prev.pop_back();
+                }
             }
-
             level++;
         }
     }
