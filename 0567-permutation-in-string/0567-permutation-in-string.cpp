f5932844a43be002d2f7934347759898128e6514
Mayank Jha <154996743+Mayank87426@users.noreply.github.com>
2026-07-29 12:41:19 +0530

    Time: 99 ms (22.89%), Space: 24.8 MB (10.09%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
+    string find_freqVector(string& s, int i, int j) {
+        string str = "";
+        vector<int> freq(26, 0);
+        for (int k = i; k <= j; k++) {
+            freq[s[k] - 'a']++;
+        }
+        for (auto x : freq) {
+            str += x + '0';
+        }
+        return str;
+    }
     bool checkInclusion(string s1, string s2) {
         int n = s2.length();
         int m = s1.length();
-        sort(begin(s1), end(s1));
+        string s1Freq = find_freqVector(s1, 0, m - 1);
         int l = 0, r = 0;
         while (r < n) {
             while (l <= r && r - l + 1 > m) {
                 l++;
             }
             if (r - l + 1 == m) {
-                string str = s2.substr(l, r - l + 1);
-                sort(begin(str), end(str));
-                if (str == s1)
+                string s2Freq = find_freqVector(s2, l,r);
+                if (s1Freq == s2Freq)
                     return true;
             }
             r++;
